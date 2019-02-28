31d64ec9d52e80aa027477998cb8d6d94972fd9d
朱贺 <zhuhe@zhuhedeMacBook-Pro.local>
2019-02-28 22:32:52 +0800

    fix: 添加config keys sessionKey

--- config/default.js
+++ config/default.js
@@ -3,6 +3,8 @@ module.exports = {
   app: {
     debug: false,
     port: 8088,
+    keys: ['keys-blog-admin', 'keykeys-blog-admin'],
+    sessionKey: 'session-key',
   },
   db: {
     URL: 'mongodb://localhost/reactBlog'
